36c1fa3a3aded458bc3294de6f014e12948d2d37
Stephan Osthold <osthold@modell-aachen.de>
2014-06-12 14:27:49 +0200

    Added JavaScript to de/select checkboxes.
    For use with PublishPluginModacCBDialogViewTemplate.
    Add the 'selectAllTopics' or 'selectNoTopics' to your button.
    
    Ticket: #4911

--- pub/System/PublishPlugin/searchtopics_src.js
+++ pub/System/PublishPlugin/searchtopics_src.js
@@ -17,6 +17,14 @@ jQuery(function($){
         $this.find('input[name="topiclist"]').val(topics);
         $this.find('input[name="catpdf"]').val(pdfs);
     });
+    $('.selectAllTopics').click(function(){
+        $('form[name="publish"]').find('input[name="topiclistCB"]').prop('checked', true);
+        return false;
+    });
+    $('.selectNoTopics').click(function(){
+        $('form[name="publish"]').find('input[name="topiclistCB"]').prop('checked', false);
+        return false;
+    });
     $('form[name="publish"] select[name="formselection"]').livequery(function(){$(this).change(function(){
         var $this = $(this);
         var $div = $this.closest('.topicselection');
