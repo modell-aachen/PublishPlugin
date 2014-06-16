21e0e6fc1676ca7b7aa5963a76b51c5291c118e3
Stephan Osthold <osthold@modell-aachen.de>
2014-06-16 17:01:13 +0200

    Livequery for Select all/none buttons
    Those will be re-fetched by ajax.
    
    Ticket: #4911

--- pub/System/PublishPlugin/searchtopics_src.js
+++ pub/System/PublishPlugin/searchtopics_src.js
@@ -17,14 +17,14 @@ jQuery(function($){
         $this.find('input[name="topiclist"]').val(topics);
         $this.find('input[name="catpdf"]').val(pdfs);
     });
-    $('.selectAllTopics').click(function(){
+    $('.selectAllTopics').livequery(function(){$(this).click(function(){
         $('form[name="publish"]').find('input[name="topiclistCB"]').prop('checked', true);
         return false;
-    });
-    $('.selectNoTopics').click(function(){
+    });});
+    $('.selectNoTopics').livequery(function(){$(this).click(function(){
         $('form[name="publish"]').find('input[name="topiclistCB"]').prop('checked', false);
         return false;
-    });
+    });});
     $('form[name="publish"] .topicselection .ajaxOnChange').livequery(function(){$(this).change(function(){
         var $this = $(this);
         var $div = $this.closest('.topicselection');
