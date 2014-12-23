f6a7bbc5895c917e8f36bc6e4ce41918838b7c90
Stephan Osthold <osthold@modell-aachen.de>
2014-12-23 15:04:33 +0100

    Using beforeSerialize for StrikeOne integration with AjaxForm
    Much easier.
    
    Ticket: #6646

--- pub/System/PublishPlugin/publishplugin_src.js
+++ pub/System/PublishPlugin/publishplugin_src.js
@@ -10,16 +10,9 @@ jQuery(function($) {
     });
 
     if($pform.ajaxForm) $pform.ajaxForm({
-        beforeSubmit: function(arr, $form, options) {
+        beforeSerialize: function($form, options) {
             if(StrikeOne !== undefined) {
                 StrikeOne.submit($form[0]);
-                for(var i = 0; i < arr.length; i++) {
-                    var obj = arr[i];
-                    if(obj.name = 'validation_key') {
-                        obj.value = $form.find('input[name=validation_key]:first').attr('value');
-                        break;
-                    }
-                }
             }
             if($.blockUI !== undefined) $.blockUI();
         },
